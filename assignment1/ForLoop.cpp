a1762d2c3754d688e314c83af387a864bdfbf83a
muskankumari11 <111870054+muskankumari11@users.noreply.github.com>
2022-09-21 15:16:54 +0530

    Create ForLoop.cpp

---
+++ assignment1/ForLoop.cpp
@@ -0,0 +1,49 @@
+//https://www.hackerrank.com/challenges/c-tutorial-for-loop/problem?isFullScreen=true
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    int a,b;
+    cin>>a;
+    cin>>b;
+    for(a;a<=b;a++)
+    {
+        if(a==1)
+        
+            cout<<"one\n";
+        
+       else if(a==2)
+        
+            cout<<"two\n";
+       else if(a==3)
+       cout<<"three\n";
+    
+    else if(a==4)
+    
+        cout<<"four\n";
+    else if(a==5)
+    
+        cout<<"five\n";
+    else if(a==6)
+    
+        cout<<"six\n";
+    else if(a==7)
+    
+        cout<<"seven\n";
+    else if(a==8)
+        
+            cout<<"eight\n";
+    else if(a==9)
+    
+        cout<<"nine\n";
+    else if (a>9)
+       if(a%2!=0)
+        cout<<"odd\n";
+       else
+       cout<<"even\n";
+    
+    }
+          // Complete the code
+    return 0;
+    }
